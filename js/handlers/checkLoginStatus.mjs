d77822a35cb8c1a19b72185d07d4338f03c40861
StianL82 <stian.lilleng@gmail.com>
2024-09-03 12:47:47 +0200

    fix for show and hide class

--- js/handlers/checkLoginStatus.mjs
+++ js/handlers/checkLoginStatus.mjs
@@ -6,20 +6,26 @@ export function checkLoginStatus() {
   if (token) {
     document.querySelectorAll(".hideIfLoggedIn").forEach((el) => {
       el.style.setProperty("display", "none", "important");
+      el.classList.remove("hidden-until-js"); // Fjern skjulingsklassen
     });
     document.querySelectorAll(".showIfLoggedInBlock").forEach((el) => {
       el.style.setProperty("display", "block", "important");
+      el.classList.remove("hidden-until-js"); // Fjern skjulingsklassen
     });
     document.querySelectorAll(".showIfLoggedInFlex").forEach((el) => {
       el.style.setProperty("display", "flex", "important");
+      el.classList.remove("hidden-until-js"); // Fjern skjulingsklassen
     });
   } else {
     document.querySelectorAll(".hideIfLoggedIn").forEach((el) => {
       el.style.setProperty("display", "block", "important");
+      el.classList.remove("hidden-until-js"); // Fjern skjulingsklassen
     });
-    document.querySelectorAll(".showIfLoggedInBlock, .showIfLoggedInFlex").forEach((el) => {
-      el.style.setProperty("display", "none", "important");
-    });
+    document
+      .querySelectorAll(".showIfLoggedInBlock, .showIfLoggedInFlex")
+      .forEach((el) => {
+        el.style.setProperty("display", "none", "important");
+        el.classList.remove("hidden-until-js"); // Fjern skjulingsklassen
+      });
   }
 }
-
