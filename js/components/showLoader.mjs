7ec4e3d55778effb26ba3ffff8093b352e47ecc5
StianL82 <stian.lilleng@gmail.com>
2024-09-12 15:09:44 +0200

    Create showLoader.mjs

---
+++ js/components/showLoader.mjs
@@ -0,0 +1,4 @@
+export function showLoadingIndicator() {
+  const loadingIndicator = document.querySelector(".loading-indicator");
+  if (loadingIndicator) loadingIndicator.style.display = "block";
+}
